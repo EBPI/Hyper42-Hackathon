3d83d0aa7f33b4f129ee616eac951adde6b70a26
John Renne <john@Johns-MBP.conx.lan>
2018-04-06 21:17:44 +0200

    Aanleveren is een transactie geworden

--- blockchain/hyperpoort/lib/logic.js
+++ blockchain/hyperpoort/lib/logic.js
@@ -18,3 +18,32 @@ function doUpdateStatus(UpdateStatus) {
             aanleveringRegistry.update(aangeleverdBericht) 
         });
 }
+
+/**
+ *  Aanleveren
+ * @param {org.ebpi.hackathon.Aanleveren} Aanleveren
+ * @transaction
+ */
+function doAanlevering(Aanleveren) {
+    var newAanleveraar = getCurrentParticipant()
+    var newStatus = 100
+    var newAanleverkenmerk = Aanleveren.AanleverKenmerk
+    var newHash=Aanleveren.Hash
+    return getParticipantRegistry('org.ebpi.hackathon.OntvangendePartij')
+    .then(function (OntvangendePartijRegistry){
+        var newOntvangendePartij = OntvangendePartijRegistry.get(Aanleveren.OntvangerId).then(function(value) {
+            var factory = getFactory()
+            var newAanlevering = factory.newResource('org.ebpi.hackathon', 'Aanlevering', newAanleverkenmerk)  
+            newAanlevering.Hash = newHash
+            newAanlevering.status = newStatus 
+            newAanlevering.Aanleveraar = newAanleveraar
+            newAanlevering.Ontvanger = newOntvangendePartij
+            return getAssetRegistry('org.ebpi.hackathon.Aanlevering')
+            .then(function(aanleveringRegistry){
+            return aanleveringRegistry.addAll([newAanlevering])
+    
+        })
+
+    })
+    })
+}
