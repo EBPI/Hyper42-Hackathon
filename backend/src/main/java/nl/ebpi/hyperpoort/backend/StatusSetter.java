b8e6941eb7328568fb73ce1dbacee1ab2fa54688
Rogier Morsink <rogier.morsink@ebpi.nl>
2018-04-06 19:19:18 +0200

    StatusSetter en UpdatStatus gemaakt

---
+++ backend/src/main/java/nl/ebpi/hyperpoort/backend/StatusSetter.java
@@ -0,0 +1,38 @@
+package nl.ebpi.hyperpoort.backend;
+
+import nl.ebpi.hyperpoort.backend.hyperledger.UpdateStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Service
+public class StatusSetter {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public boolean setStatus(String kenmerk, String newStatus, String aanleveraar) throws URISyntaxException {
+        UpdateStatus updateStatus = new UpdateStatus();
+        updateStatus.setKenmerk(kenmerk);
+        updateStatus.setStatus(newStatus);
+        updateStatus.setAanleveraar(aanleveraar);
+        URI uri = new URI("asd");
+
+        RequestEntity<UpdateStatus> requestEntity = new RequestEntity<UpdateStatus>(HttpMethod.POST, uri);
+        ResponseEntity<Void> responseEntity = restTemplate.exchange(requestEntity, void.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return true;
+        }
+
+
+        return false;
+
+    }
+
+
+}
