cc4f3155b3b493ce925701e4bbd021c9dd0de3fc
Micha Wensveen <micha.wensveen@ebpi.nl>
2018-04-06 17:41:34 +0200

    Poller met test

--- backend/src/test/java/nl/ebpi/hyperpoort/backend/thread/PollerTest.java
+++ backend/src/test/java/nl/ebpi/hyperpoort/backend/thread/PollerTest.java
@@ -4,6 +4,7 @@ import nl.ebpi.hyperpoort.backend.hyperledger.Aanlevering;
 import org.easymock.Capture;
 import org.easymock.EasyMock;
 import org.easymock.IMocksControl;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -26,14 +27,39 @@ public class PollerTest {
 	}
 
 	@Test
-	public void testRun() throws Exception {
-		fixture.setAanleverKenmerk("aanleverkenmer");
-		Capture<RequestEntity> requestCapture = Capture.newInstance();
-		ResponseEntity<Aanlevering> response = new ResponseEntity<>(new Aanlevering(), HttpStatus.OK);
+	public void testFindInFirstTry() throws Exception {
+		fixture.setAanleverKenmerk("aanleverkenmerk");
+		Capture<RequestEntity<String>> requestCapture = Capture.newInstance();
+		Aanlevering aanlevering = new Aanlevering();
+		ResponseEntity<Aanlevering> response = new ResponseEntity<>(aanlevering, HttpStatus.OK);
+
 		EasyMock.expect(restTemplate.exchange(EasyMock.capture(requestCapture), EasyMock.anyObject(Class.class))).andReturn(response);
+
 		mocksControl.replay();
 		fixture.run();
 		mocksControl.verify();
+
+		Assert.assertEquals("aanleverkenmerk", requestCapture.getValue().getBody());
+		Assert.assertEquals(aanlevering, ReflectionTestUtils.getField(fixture, "aanlevering"));
+	}
+
+	@Test
+	public void testFindInSecondTry() throws Exception {
+		fixture.setAanleverKenmerk("aanleverkenmerk");
+		Capture<RequestEntity<String>> requestCapture = Capture.newInstance();
+		Aanlevering aanlevering = new Aanlevering();
+		ResponseEntity<Aanlevering> responseEmpy = new ResponseEntity<>(null, HttpStatus.OK);
+		ResponseEntity<Aanlevering> response = new ResponseEntity<>(aanlevering, HttpStatus.OK);
+
+		EasyMock.expect(restTemplate.exchange(EasyMock.capture(requestCapture), EasyMock.anyObject(Class.class))).andReturn(responseEmpy);
+		EasyMock.expect(restTemplate.exchange(EasyMock.capture(requestCapture), EasyMock.anyObject(Class.class))).andReturn(response);
+
+		mocksControl.replay();
+		fixture.run();
+		mocksControl.verify();
+
+		Assert.assertEquals("aanleverkenmerk", requestCapture.getValue().getBody());
+		Assert.assertEquals(aanlevering, ReflectionTestUtils.getField(fixture, "aanlevering"));
 	}
 
 }
