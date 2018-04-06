8b9b517d0191e9e26f83e83fdf36f7b8e085e0da
Micha Wensveen <micha.wensveen@ebpi.nl>
2018-04-06 22:47:55 +0200

    fix request

--- backend/src/main/java/nl/ebpi/hyperpoort/backend/BackendController.java
+++ backend/src/main/java/nl/ebpi/hyperpoort/backend/BackendController.java
@@ -4,19 +4,19 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.UUID;
 import nl.ebpi.hyperpoort.backend.model.Aanlevering;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hyperpoort")
 public class BackendController {
 
-	@PostMapping("/aanlever")
-	public String aanleveren(@RequestParam Aanlevering aanlevering) throws URISyntaxException, IOException {
+	@PostMapping(value = "/aanleveren", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String aanleveren(@RequestBody Aanlevering aanlevering) throws URISyntaxException, IOException {
 		String kenmerk = UUID.randomUUID().toString();
-		// Start Thread die polt met kenmerk
 
 		return kenmerk;
 	}
