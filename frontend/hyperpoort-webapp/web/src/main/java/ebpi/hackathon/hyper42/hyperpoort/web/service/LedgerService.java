2f89765556c62b2f1e67694dacba83032d973617
kkerkhoven-ebpi <kevin.kerkhoven@ebpi.nl>
2018-04-07 13:59:25 +0200

    Gebruik juiste session, parse kvk nummer, maak alvast aan te roepen methode

---
+++ frontend/hyperpoort-webapp/web/src/main/java/ebpi/hackathon/hyper42/hyperpoort/web/service/LedgerService.java
@@ -0,0 +1,10 @@
+package ebpi.hackathon.hyper42.hyperpoort.web.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LedgerService {
+	public byte[] makeCard(long kvkNumber) {
+		return "dummy".getBytes();
+	}
+}
