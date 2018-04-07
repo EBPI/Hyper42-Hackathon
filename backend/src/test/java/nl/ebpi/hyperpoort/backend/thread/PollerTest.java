3582e02b79e697ef40a47cf006bb3054221937a7
Micha Wensveen <micha.wensveen@ebpi.nl>
2018-04-07 14:04:53 +0200

    Poller refactoring

--- backend/src/test/java/nl/ebpi/hyperpoort/backend/thread/PollerTest.java
+++ backend/src/test/java/nl/ebpi/hyperpoort/backend/thread/PollerTest.java
@@ -1,38 +1,31 @@
 package nl.ebpi.hyperpoort.backend.thread;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import nl.ebpi.hyperpoort.backend.FinalStatusSetter;
-import nl.ebpi.hyperpoort.backend.StatusSetter;
 import nl.ebpi.hyperpoort.backend.hyperledger.Aanlevering;
+import nl.ebpi.hyperpoort.backend.hyperledger.AanleveringenGetter;
+import nl.ebpi.hyperpoort.backend.hyperledger.FinalStatusSetter;
+import nl.ebpi.hyperpoort.backend.hyperledger.StatusSetter;
 import org.easymock.Capture;
+import org.easymock.CaptureType;
 import org.easymock.EasyMock;
 import org.easymock.IMocksControl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.client.RestTemplate;
 
 public class PollerTest {
 	private final IMocksControl mocksControl = EasyMock.createControl();
-	private RestTemplate restTemplate;
 	private Poller fixture;
 	private StatusSetter statusSetter;
 	private FinalStatusSetter finalStatusSetter;
+	private AanleveringenGetter aanleveringenGetter;
 
 	@Before
 	public void createPoller() throws Exception {
 		fixture = new Poller();
 
-		restTemplate = mocksControl.createMock(RestTemplate.class);
-		ReflectionTestUtils.setField(fixture, "restTemplate", restTemplate);
+		aanleveringenGetter = mocksControl.createMock(AanleveringenGetter.class);
+		ReflectionTestUtils.setField(fixture, "aanleveringenGetter", aanleveringenGetter);
 
 		statusSetter = mocksControl.createMock(StatusSetter.class);
 		ReflectionTestUtils.setField(fixture, "statusSetter", statusSetter);
@@ -42,21 +35,15 @@ public class PollerTest {
 	}
 
 	@Test
-	public void testFindInFirstTry() throws Exception {
+	public void testPoll() throws Exception {
 		fixture.setAanleverKenmerk("aanleverkenmerk");
 		Aanlevering aanlevering = new Aanlevering();
 		aanlevering.setAanleveraar("aanleveraar");
-		ResponseEntity<List<Aanlevering>> response = new ResponseEntity<>(Collections.singletonList(aanlevering), HttpStatus.OK);
 
-		Capture<String> urlCapture = Capture.newInstance();
-		Capture<HttpMethod> httpMethodeCapture = Capture.newInstance();
-		Capture<HttpEntity<?>> entityCapture = Capture.newInstance();
-		Capture<ParameterizedTypeReference<List<Aanlevering>>> aanleveringenCapture = Capture.newInstance();
-		EasyMock.expect(restTemplate.exchange(EasyMock.capture(urlCapture), EasyMock.capture(httpMethodeCapture), EasyMock.capture(entityCapture),
-				EasyMock.capture(aanleveringenCapture), EasyMock.anyObject(Map.class))).andReturn(response);
+		Capture<String> aanleverKenmerkCapture = Capture.newInstance(CaptureType.ALL);
+		EasyMock.expect(aanleveringenGetter.retrieveAanlevering(EasyMock.capture(aanleverKenmerkCapture))).andReturn(aanlevering);
 
-		Capture<String> aanleverKenmerkCapture = Capture.newInstance();
-		Capture<String> statusCapture = Capture.newInstance();
+		Capture<String> statusCapture = Capture.newInstance(CaptureType.ALL);
 		Capture<String> aanleveraarCapture = Capture.newInstance();
 		EasyMock.expect(statusSetter.setStatus(EasyMock.capture(aanleverKenmerkCapture), EasyMock.capture(statusCapture), EasyMock.capture(aanleveraarCapture)))
 				.andReturn(Boolean.TRUE);
@@ -68,43 +55,14 @@ public class PollerTest {
 		fixture.run();
 		mocksControl.verify();
 
-		Assert.assertEquals(aanlevering, ReflectionTestUtils.getField(fixture, "aanlevering"));
-	}
+		Assert.assertEquals(3, aanleverKenmerkCapture.getValues().size());
+		Assert.assertEquals("aanleverkenmerk", aanleverKenmerkCapture.getValues().get(0));
+		Assert.assertEquals("aanleverkenmerk", aanleverKenmerkCapture.getValues().get(1));
+		Assert.assertEquals("aanleverkenmerk", aanleverKenmerkCapture.getValues().get(2));
 
-	@Test
-	public void testFindInSecondTry() throws Exception {
-		ResponseEntity<Aanlevering> responseEmpty = new ResponseEntity<>(null, HttpStatus.OK);
-		fixture.setAanleverKenmerk("aanleverkenmerk");
-		Aanlevering aanlevering = new Aanlevering();
-		List<Aanlevering> aanleveringen = Collections.singletonList(aanlevering);
-		aanlevering.setAanleveraar("aanleveraar");
-		ResponseEntity<List<Aanlevering>> response = new ResponseEntity<>(aanleveringen, HttpStatus.OK);
-
-		Capture<String> urlCapture = Capture.newInstance();
-		Capture<HttpMethod> httpMethodeCapture = Capture.newInstance();
-		Capture<HttpEntity<?>> entityCapture = Capture.newInstance();
-
-		Capture<ParameterizedTypeReference<List<Aanlevering>>> aanleveringenCapture = Capture.newInstance();
-		EasyMock.expect(restTemplate.exchange(EasyMock.capture(urlCapture), EasyMock.capture(httpMethodeCapture), EasyMock.capture(entityCapture),
-				EasyMock.capture(aanleveringenCapture), EasyMock.anyObject(Map.class))).andReturn(responseEmpty);
-		EasyMock.expect(restTemplate.exchange(EasyMock.capture(urlCapture), EasyMock.capture(httpMethodeCapture), EasyMock.capture(entityCapture),
-				EasyMock.capture(aanleveringenCapture), EasyMock.anyObject(Map.class))).andReturn(response);
-
-		Capture<String> aanleverKenmerkCapture = Capture.newInstance();
-		Capture<String> statusCapture = Capture.newInstance();
-		Capture<String> aanleveraarCapture = Capture.newInstance();
-		EasyMock.expect(statusSetter.setStatus(EasyMock.capture(aanleverKenmerkCapture = Capture.newInstance()),
-				EasyMock.capture(statusCapture = Capture.newInstance()),
-				EasyMock.capture(aanleveraarCapture))).andReturn(Boolean.TRUE);
-
-		EasyMock.expect(finalStatusSetter.setFinalStatus(EasyMock.capture(aanleverKenmerkCapture), EasyMock.capture(statusCapture),
-				EasyMock.capture(aanleveraarCapture))).andReturn(Boolean.TRUE);
-
-		mocksControl.replay();
-		fixture.run();
-		mocksControl.verify();
-
-		Assert.assertEquals(aanlevering, ReflectionTestUtils.getField(fixture, "aanlevering"));
+		Assert.assertEquals(2, statusCapture.getValues().size());
+		Assert.assertEquals("200", statusCapture.getValues().get(0));
+		Assert.assertEquals("500", statusCapture.getValues().get(1));
 	}
 
 }
